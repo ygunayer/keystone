bde2ff648ac8abdcc56ca65f447b81255493eff7
Yalin Gunayer <yalin.gunayer@emlakjet.com>
2016-04-20 16:22:52 +0300

    Display disabled URL fields as links

--- fields/types/url/UrlField.js
+++ fields/types/url/UrlField.js
@@ -1,5 +1,10 @@
 var Field = require('../Field');
+var React = require('react');
 
 module.exports = Field.create({
-	displayName: 'UrlField'
+	displayName: 'UrlField',
+
+    renderValue: function() {
+        return <a href={this.props.value} target='_blank'>{this.props.value}</a>;
+    }
 });
